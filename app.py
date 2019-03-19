383713010816c573d75a17e346c872acaf87551a
zyMarshtomp <43492391+zyMarshtomp@users.noreply.github.com>
2019-03-19 20:37:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,7 @@ def pre_pic(picName):
     im_arr = np.array(reIm.convert('L'))
     threshould = 50  # 噪点控制
     im_arr_num = []  # 偶数位储存字符开始位置, 奇数位储存字符结束位置
-    space = 0  # 存储空白像素
-    black = True  # 黑白检测模式切换(先是黑检测)
 
-	# 图片转化为黑白二色, 并去除噪点(50) + 反相
     for i in range(28):
         for j in range(28):
             im_arr[i][j] = 255 - im_arr[i][j]
@@ -51,26 +48,17 @@ def pre_pic(picName):
             else:
                 im_arr[i][j] = 255
 
-    # 假定每个数字间有空隙, 开始出现黑色即为出现字符, 记录位置; 全列空白即为字符结束, 记录位置
-    # 单行列切片
-    for row in range(28):
-        for i in range(28):
-            if black:
-                # 黑检测模式(检测是否出现数字)
-                # 出现黑色即为出现字符
-                if im_arr[i][row] == 0:
-                    im_arr_num.append(row)
-                    black = False  # 切换白检测模式
-                    continue
-            else:
-                # 白检测模式(数字结束)
-                if im_arr[i][row] == 255:
-                    space = space + 1
-            if space == 28:
-                im_arr_num.append(row)
-                black = True  # 切换黑模式
-            # space归零
-            space = 0
+    black = True  # 黑白检测模式切换(先是黑检测)
+    for col in range(im_arr.shape[1]):
+        # 对第col列求和，如果该列全部为0则为全白，否则有黑色。
+        if black and sum(im_arr[:, col]) != 0:
+            im_arr_num.append(col)
+            black = False
+        elif not black and sum(im_arr[:, col]) == 0:
+            im_arr_num.append(col)
+            black = True
+    if len(im_arr_num) % 2 == 1:
+        im_arr_num.append(len(im_arr.shape[1]))
 
     # 每个数字进行截取, resize, reshape并返回img_ready
     img_ready = []  # 存放多个数字
@@ -78,6 +66,7 @@ def pre_pic(picName):
     assert len(im_arr_num) % 2 == 0  # 开始结束成对出现：im_arr_num是偶数，否则错误
 
     # im_arr:原图像
+    # im_arr_num:
     # 第一个数字开始在0，结束在1
     # 每个数字步长为2
     for num in range(0, len(im_arr_num), 2):
@@ -86,16 +75,8 @@ def pre_pic(picName):
         cur_detect = im_arr[:, im_arr_num[num]:im_arr_num[num + 1] + 1]
         cur_detect = Image.fromarray(cur_detect)  # 转为图片
         cur_detect = cur_detect.resize((28, 28), Image.ANTIALIAS)  # 图片resize
-
-################################################################
-
-	nm_arr = cur_detect.reshape([1, 784])
-	nm_arr = nm-arr.astype(np.float32)	
-	cur_ready = np.multiply(cur_detect, 1.0/255.0)
-
-################################################################
-
-        img_ready.append(cur_ready)  # 加入结果list
+        cur_detect = cur_detect.resize([784, 1])
+        img_ready.append(cur_detect)  # 加入结果list
     return img_ready
 
 
