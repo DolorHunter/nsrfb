47a85e6da3635d7257d4ae208461d7fb87d2f5c7
DolorHunter <Dolorhunter@gmail.com>
2019-04-08 10:57:36 +0800

    04.08.10.57

--- app_gui.py
+++ app_gui.py
@@ -1,24 +1,62 @@
 # coding:utf-8
 
-# This is a test gui
 
 import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
 from PIL import Image, ImageTk
+import PIL
 
 
-def center_window(w=300, h=200):
-    ws = root.winfo_screenwidth()
-    hs = root.winfo_screenheight()
-    x = (ws / 2) - (w / 2)
-    y = (hs / 2) - (h / 2)
-    root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+class App:
+    path = "abc"
+    # path = StringVar()  # 文件夹路径
+    #file_name =
+    #img = self.show_img(file_name)
 
-    # 打开图片
-    img_open = tk.PhotoImage(file="pic/hk_check.gif")
-    label_img = tk.Label(root, image=img_open)
-    label_img.pack()
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.path = StringVar()  # 文件夹路径
+        self.path_label = Label(frame, text="目标路径:")
+        self.path_label.pack(side=LEFT)
+        self.path_entry = Entry(frame, textvariable=self.path)
+        self.path_entry.pack(side=LEFT)
+        self.path_button = Button(frame, text="浏览文件", command=self.select_file)
+        self.path_button.pack(side=LEFT)
+        self.path_button = Button(frame, text="选择路径", command=self.select_path)
+        self.path_button.pack(side=LEFT)
 
+        # self.img_label = Label(frame, image=self.tkImage)
+        # self.path_button.pack()
 
-root = tk.Tk(className="基于卷积神经网络识别金融票据中的数字串")
-center_window(500, 300)
+        self.quit_button = Button(frame, text="  QUIT  ", command=quit)
+        self.quit_button.pack(side=RIGHT)
+
+    def center_window(self, w=300, h=200):
+        ws = root.winfo_screenwidth()
+        hs = root.winfo_screenheight()
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
+    def select_file(self):
+        file_name = filedialog.askopenfile()
+        return file_name
+
+    def select_path(self):
+        img_path = filedialog.askdirectory()
+        self.path.set(img_path)
+
+'''
+    def show_img(self, file_name):
+        # img = self.select_file()
+        img = Image.open("pic/0.png")
+        tkImage = ImageTk.PhotoImage(image=img)
+        return tkImage
+'''
+
+root = Tk(className="基于卷积神经网络识别金融票据中的数字串")
+app = App(root)
+app.center_window(500, 300)
 root.mainloop()
