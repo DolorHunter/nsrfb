d98cdc6ef66b267a130c5e1af08ee034a0164080
DolorHunter <Dolorhunter@gmail.com>
2019-04-08 12:40:06 +0800

    04.08.12.39 - GUI framework

--- app_gui.py
+++ app_gui.py
@@ -5,32 +5,29 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
-import PIL
+import app
 
 
 class App:
-    path = "abc"
-    # path = StringVar()  # 文件夹路径
-    #file_name =
-    #img = self.show_img(file_name)
-
     def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-        self.path = StringVar()  # 文件夹路径
-        self.path_label = Label(frame, text="目标路径:")
+        self.frame = Frame(master)
+        self.frame.pack()
+        # TEST!!
+        self.file_path = StringVar()     # 文件夹路径
+        self.file_name = StringVar()     # 文件名
+        self.img_label = StringVar()     # 图片
+        self.date_label = StringVar()    # 预测的日期
+        self.amount_label = StringVar()  # 预测的金额
+        # TEST!!
+        self.path_label = Label(self.frame, text="目标路径:")
         self.path_label.pack(side=LEFT)
-        self.path_entry = Entry(frame, textvariable=self.path)
+        self.path_entry = Entry(self.frame, textvariable=self.file_path)
         self.path_entry.pack(side=LEFT)
-        self.path_button = Button(frame, text="浏览文件", command=self.select_file)
+        self.path_button = Button(self.frame, text="浏览文件", command=self.select_file)
         self.path_button.pack(side=LEFT)
-        self.path_button = Button(frame, text="选择路径", command=self.select_path)
+        self.path_button = Button(self.frame, text="选择路径", command=self.select_path)
         self.path_button.pack(side=LEFT)
-
-        # self.img_label = Label(frame, image=self.tkImage)
-        # self.path_button.pack()
-
-        self.quit_button = Button(frame, text="  QUIT  ", command=quit)
+        self.quit_button = Button(self.frame, text="  QUIT  ", command=quit)
         self.quit_button.pack(side=RIGHT)
 
     def center_window(self, w=300, h=200):
@@ -41,20 +38,36 @@ class App:
         root.geometry("%dx%d+%d+%d" % (w, h, x, y))
 
     def select_file(self):
-        file_name = filedialog.askopenfile()
-        return file_name
+        self.file_name = filedialog.askopenfile()
 
     def select_path(self):
-        img_path = filedialog.askdirectory()
-        self.path.set(img_path)
+        self.file_path = filedialog.askdirectory()
+        self.file_path.set(self.file_path)
 
-'''
-    def show_img(self, file_name):
-        # img = self.select_file()
-        img = Image.open("pic/0.png")
-        tkImage = ImageTk.PhotoImage(image=img)
-        return tkImage
-'''
+    def open_image(self):
+        open_img = Image.open(self.file_name)
+        img = ImageTk.PhotoImage(image=open_img)  # 处理格式
+        self.img_label = Label(self.frame, text="Image", image=img)
+        self.img_label.pack(side=LEFT)
+
+    def prt_date(self):
+        cut_images = app.capture_date(self.file_name)
+        date = []
+        for img in cut_images:
+            detect_number = app.restore_model(img)
+            date.append(detect_number)
+        self.date_label = Label(self.frame, text=date)
+        self.date_label.pack(side=LEFT)
+
+    def prt_amount(self):
+        cut_images = app.capture_amount(self.file_name)
+        amount = []
+        for img in cut_images:
+            detect_number = app.restore_model(img)
+            amount.append(detect_number)
+        self.amount_label = Label(self.frame, text=amount)
+        self.amount_label.pack(side=LEFT)
+
 
 root = Tk(className="基于卷积神经网络识别金融票据中的数字串")
 app = App(root)
